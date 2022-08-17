e4cecf440b5402adc4ff71533c8015b3f7276803
Gayathri S S <ssgayathri2001@gmail.com>
2022-08-17 13:18:58 +0530

    Add project files.

---
+++ 2nd_day_demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_day_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            float radius;
+            float pi = 3.14f;
+            Console.WriteLine("enter radius");
+            radius = Convert.ToSingle(Console.ReadLine());
+
+            double area = pi * radius * radius;
+            Console.WriteLine(area);
+            Console.ReadLine();
+        }
+    }
+}
